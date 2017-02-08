c6ca41a0321d2aeb18d3a04a5ada763bfbc4ffb3
Alex Guedes <alex.guedes@gmail.com>
2017-02-08 13:57:57 +0000

    Added information about service when it gets bind-ed.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, json
 from socket import gethostname
 from os import getenv
 
@@ -7,8 +7,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify({'message': 'hello world.', 'hostname': gethostname()}), 200
+    vcap_services = getenv('VCAP_SERVICES')
+    return jsonify({'message': 'hello world.', 'hostname': gethostname(), 'services-bind': json.loads(vcap_services)}), 200
 
-port = getenv('VCAP_APP_PORT', '5000') 
+
+port = getenv('VCAP_APP_PORT', '5000')
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=int(port), threaded=True)
+    app.run(debug=False, host='0.0.0.0', port=port, threaded=True)
